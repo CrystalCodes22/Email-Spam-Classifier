32b5b17090550143edb9ad5514cb381c5d9d81c6
Rrishi Anand <lichupatnaik@gmail.com>
2020-08-07 09:35:52 -0700

    small staf

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import requests
 import json
-  
-def get_prediction():
+
+
+def get_prediction(data):
     url = 'https://l47syleshe.execute-api.us-east-1.amazonaws.com/Predict/88b81708-fbb3-4e39-a70d-619326e202bc'
     r = requests.post(url, data=json.dumps(data))
     response = getattr(r,'_content').decode("utf-8")
     return response
 
 content = input("What was the email you got?")
-uniqeW = input("How many unique words are there?")
-totalW = input("How many words are their total?")
+words_list = content.split()
+uniqeW = len(words_list)
+totalW = len(set(words_list))
 
-prediction = get_prediction(data={"content":content,"num_unique_words":uniqeW,"total_num_words":totalW})
+prediction = json.loads(json.loads(get_prediction({"content":content,"num_unique_words":uniqeW,"total_num_words":totalW}))['body'])['predicted_label']
+print(prediction)
